782a54671e3581b719b2c67670e1ff81a1f1f513
Utpal Paul <utpalseu@gmail.com>
2021-06-11 18:43:44 +0600

    UPDATED : Contact Service Provider

--- src/Providers/ContactServiceProvider.php
+++ src/Providers/ContactServiceProvider.php
@@ -13,7 +13,7 @@
         public function register()
         {
             $this->mergeConfigFrom(
-                __DIR__.'/config/contact.php', 'contact'
+                __DIR__.'/../config/contact.php', 'contact'
             );
         }
 
@@ -24,13 +24,13 @@
          */
         public function boot()
         {
-            $this->loadRoutesFrom(__DIR__.'/routes/web.php');
-            $this->loadViewsFrom(__DIR__.'/views', 'contact');
-            $this->loadMigrationsFrom(__DIR__.'/database/migrations');
+            $this->loadRoutesFrom(__DIR__.'/../routes/web.php');
+            $this->loadViewsFrom(__DIR__.'/../views', 'contact');
+            $this->loadMigrationsFrom(__DIR__.'/../database/migrations');
 
             $this->publishes([
-                __DIR__.'/views' => resource_path('views/vendor/contact'),
-                __DIR__.'/config/contact.php' => config_path('contact.php')
+                __DIR__.'/../views' => resource_path('views/vendor/contact'),
+                __DIR__.'/../config/contact.php' => config_path('contact.php')
             ]);
         }
     }
